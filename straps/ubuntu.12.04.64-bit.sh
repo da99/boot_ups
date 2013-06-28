3ed8f222268ce28f2d3ceb6c3428c3d094bb5af6
da99 <i-hate-spam-1234567@mailinator.com>
2013-06-28 03:01:28 -0500

    Dev. checkpoint.

--- straps/ubuntu.12.04.64-bit.sh
+++ straps/ubuntu.12.04.64-bit.sh
@@ -1,17 +1,30 @@
-#!/usr/bin/env bash
 # -*- bash -*-
 #
 set -u -e -o pipefail
 
+# =========== Update/Upgrade =========================
 sudo apt-get update
+
+# http://www.cgurnik.com/2011/02/20/fixing-perl-warning-setting-locale-failed-in-ubuntu/
+sudo dpkg-reconfigure locales
+
 sudo apt-get upgrade
-sudo apt-get install htop
+sudo apt-get dist-upgrade
+
+# =========== Common Programs =========================
+sudo apt-get install build-essential libssl-dev python-software-properties python g++ make
+sudo apt-get install software-properties-common
+sudo apt-get install git curl htop atop
+
+
+# =========== Optional: Removed apache2 ===============
 sudo apt-get remove apache2*
 sudo apt-get purge apache2*
 sudo apt-get autoremove
 
-sudo apt-get dist-upgrade
 
-#!/usr/bin/env node
-# -*- js   -*-
-#
+# =========== Optional: Install node.js ===============
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install nodejs
+
