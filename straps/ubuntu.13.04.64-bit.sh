6f988c0f709dc064e776622813e5a8460fc00cb6
da99 <i-hate-spam-1234567@mailinator.com>
2013-06-30 02:06:20 -0500

    Merged common functionality.

--- straps/ubuntu.13.04.64-bit.sh
+++ straps/ubuntu.13.04.64-bit.sh
@@ -7,10 +7,14 @@ sudo dpkg-reconfigure locales
 
 
 # =========== Update/Upgrade =========================
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get dist-upgrade
-
+upgrade_done="/tmp/upgrade_done"
+if [[ ! -f "$upgrade_done" ]]
+then
+  sudo apt-get update
+  sudo apt-get upgrade
+  sudo apt-get dist-upgrade
+  touch "$upgrade_done"
+fi
 
 # =========== Common Programs =========================
 sudo apt-get install build-essential            \
@@ -46,15 +50,57 @@ sudo apt-get autoremove
 
 # =========== Install node.js ===============
 # =========== Install Nginx   ===============
-sudo add-apt-repository ppa:chris-lea/node.js
-sudo add-apt-repository  nginx/stable
+dev_done="/tmp/dev_done"
+if [[ ! -f "$dev_done" ]]
+then
+  sudo add-apt-repository ppa:chris-lea/node.js
+  sudo add-apt-repository  nginx/stable
 
-sudo apt-get update
+  sudo apt-get update
 
-sudo apt-get install nodejs
-sudo apt-get install nginx
+  sudo apt-get install nodejs
+  sudo apt-get install nginx
+  touch "$dev_done"
+fi
 
+# ========== Setup base ssh config for bitbucket and github
+if [[ ! -f ~/.ssh/config ]]
+then
+  wget https://raw.github.com/da99/boot_ups/master/common/ssh_config
+  mk -p ~/.ssh
+  mv ssh_config ~/.ssh/config
+fi
+
+# ==== Get user input:
+# ==== From: http://stackoverflow.com/questions/226703/how-do-i-prompt-for-input-in-a-linux-shell-script
+
+git_done="/tmp/git_done"
+if [[ ! -f "$git_done" ]]
+then
+  echo "What is the git user name?"
+  read NAME
+  git config --global user.name  "$NAME"
+
+  echo "What is the git user email?"
+  read EMAIL
+  git config --global user.email "$EMAIL"
+  touch "$git_done"
+fi
+
+
+if [[ ! -d /apps ]]
+then
+  echo "What git repo to download to /apps? ssh://[ path ]"
+  read URL
+  mkdir -p /apps
+  cd /apps
+  git clone ssh://$URL
+fi
 
 
 # ============== Goodbye ===================================================
 echo -e "\n\n ---> sendmail removed. REBOOT this machine manually. <---"
+
+
+
+
